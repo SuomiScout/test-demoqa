e1ce2a0243454fb44fa16fdfc0d2ff39c63fa4cd
SuomiScout <jarompanen@gmail.com>
2024-07-01 15:58:45 +0300

    add all

---
+++ src/test/java/junit/FirstJunitTest.java
@@ -0,0 +1,32 @@
+package junit;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+
+public class FirstJunitTest {
+
+    @BeforeAll
+
+    static void beforeAll () {
+        //Configuration.browser=
+    }
+
+    @AfterAll (){
+
+    }
+
+    @BeforeEach (){
+
+    }
+    @AfterEach (){
+
+    }
+
+
+    @Test
+    void firstTest () {
+
+        Assertions.assertTrue(3>2);
+
+    }
+}
