d8e497d35651178e0205224b97c14cd2517e8f35
SuomiScout <jarompanen@gmail.com>
2024-07-03 22:25:46 +0300

    add 03/07

--- src/test/java/demoqa/TextBox.java
+++ src/test/java/demoqa/TextBox.java
@@ -2,13 +2,13 @@ package demoqa;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static java.lang.Thread.sleep;
 
 public class TextBox {
@@ -17,14 +17,17 @@ public class TextBox {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy="eager";
     }
     @Test
     void fillFormTest () {
         String userName = "Patrick Star";
-        //open("https://www.google.ru/?hl=ru");
         open("/text-box");
-        Selenide.sleep(5000);
-        //$(".body-height").shouldHave(text("Text Box"));
+        //open("https://demoqa.com");
+        //$(Selectors.byText("Forms")).click();
+        //$(".pr-1").click();
+        //$("#item-0").click();
+        $(".body-height").shouldHave(text("Text Box"));
         $("#userName").setValue(userName);
         $("#userEmail").setValue("star666@mail.ru");
         $("#currentAddress").setValue("Bikini Bottom 666");
@@ -35,6 +38,6 @@ public class TextBox {
         $("#output").$("#name").shouldHave((text(userName)));
         $("#output #email").shouldHave((text("star666@mail.ru")));
 
-        Selenide.sleep(10000);
+        Selenide.sleep(5000);
     }
 }
