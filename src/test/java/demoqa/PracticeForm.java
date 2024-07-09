c4a2d9ee3389ad6cb13e3ed54da676de2e48b41c
SuomiScout <jarompanen@gmail.com>
2024-07-09 23:46:58 +0300

    add 09/07 fillPracticeForm

--- src/test/java/demoqa/PracticeForm.java
+++ src/test/java/demoqa/PracticeForm.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -28,6 +27,9 @@ public class PracticeForm {
     void fillPracticeForm() {
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        Selenide.executeJavaScript("$('.sidebar-content').remove()");
+        Selenide.executeJavaScript("$('footer').remove()");
+        sleep(5000);
         $("#firstName").setValue("Patrick");
         $("#userEmail").setValue("star666@mail.ru");
         $("#lastName").setValue("Star");
@@ -37,25 +39,31 @@ public class PracticeForm {
         sleep(5000);
         $("#userNumber").setValue("88005553535");
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").click();
-        $(byText("March")).click();
+        $(".react-datepicker__month-select").selectOption("March");//другой вариант
+        //$(byText("March")).click();
         $(".react-datepicker__year-select").click();
         $(byText("1961")).click();
         $(byText("13")).click();
         sleep(5000);
-        //$("#subjectsInput").setValue("Один из самых главных персонажей «Губка Боб Квадратные Штаны»");
+        $("#subjectsInput").setValue("Arts").pressEnter();
         $("#hobbies-checkbox-1").ancestor("div").click();
         File cv = new File("src/test/resources/zat.jpg");
         $("#uploadPicture").uploadFile(cv);
+        //$("#uploadPicture").uploadFile(new File("src/test/resources/zat.jpg")); //альтернативный вариант
+        //$("#uploadPicture").uploadFromClasspath("zat.jpg"); //альтернативный вариант
         $("#currentAddress").setValue("Bikini Bottom 666");
-        $(byText("Select State")).click();
-        $(byText("Rajasthan")).click();
-        $(byText("Select City")).click();
-        $(byText("Jaiselmer")).click();
+        //$(byText("Select State")).click();
+        $("#state").click();
+        //$(byText("Rajasthan")).click();
+        $("#stateCity-wrapper").$(byText("Rajasthan")).click();
+        //$(byText("Select City")).click();
+        $("#city").click();
+        //$(byText("Jaiselmer")).click();
+        $("#stateCity-wrapper").$(byText("Jaiselmer")).click();
         sleep(8000);
         $("#submit").click();
         sleep(8000);
-        $(".modal-content").shouldBe(visible);
+        $(".modal-content").should(appear);
         $(".modal-body").shouldHave((text("Patrick")));
         sleep(5000);
         $("#closeLargeModal").click();
